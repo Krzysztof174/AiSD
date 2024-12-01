f1e55a4676f248259d6017a9d2fecd68f23a2c66
Krzysztof174 <174903@student.uwm.edu.pl>
2024-12-01 18:28:41 +0100

    creating graf

--- WinFormsApp2/Graf.cs
+++ WinFormsApp2/Graf.cs
@@ -15,6 +15,7 @@ namespace WinFormsApp2
         List<Graf> Grafs = new List<Graf>();
         Graf(Edge edge)
         {
+            start = edge.start;
             Vertices.Add(edge.start);
             Vertices.Add(edge.end);
             Edges.Add(edge);
@@ -23,24 +24,46 @@ namespace WinFormsApp2
         {
             int firstVertex = edge.start.data;
             int lastVertex = edge.end.data;
-            int found = 0;
+            int count = 2;
             
             for (int i = 0; i < Vertices.Count; i++)
             {
                 if (Vertices[i].data == firstVertex)
                 {
-                    found++;
+                    count--;
                 }
                 if (Vertices[i].data == lastVertex)
                 {
-                    found++;
+                    count--;
                 }
             }
-            return found;
+            return count;
         }
         void Add(Edge edge)
         {
-            int newVerticesCount = NewVerticesCount(edge);
+            int newVerticesCount = 2;
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                if (Vertices[i].data == firstVertex)
+                {
+                    newVerticesCount--;
+                }
+                if (Vertices[i].data == lastVertex)
+                {
+                    newVerticesCount--;
+                }
+            }
+            for (int i = 0; i < Grafs.Count; i++)
+            {
+                for(int j = 0; j < Grafs[i].Vertices.Count; j++)
+                {
+                    if (Grafs[i].Vertices[j].data == edge.start.data || Grafs[i].Vertices[j].data == edge.end.data)
+                    {
+                        newVerticesCount--;
+                    }
+                }
+            }
+
             if(newVerticesCount == 2)
             {
                 Graf graf = new Graf(edge);
@@ -49,27 +72,88 @@ namespace WinFormsApp2
             }
             if(newVerticesCount == 1)
             {
-                Edges.Add(edge);
-                Boolean foundStart = false;
-                for(int i = 0; i < Vertices.Count; i++)
+                for(int i = 0; i < Vertices.Count; ++i)
                 {
-                    if (Vertices[i].data == edge.start.data) foundStart = true;
+                    if (Vertices[i].data == edge.start.data)
+                    {
+                        Edges.Add(edge);
+                        Vertices.Add(edge.end);
+                        return;
+                    }
+                    if (Vertices[i].data == edge.end.data)
+                    {
+                        Edges.Add(edge);
+                        Vertices.Add(edge.start);
+                        return;
+                    }
                 }
-                if(foundStart)
+                for (int i = 0; i < Grafs.Count; i++)
                 {
-                    Vertices.Add(edge.end);
+                    for (int j = 0; j < Grafs[i].Vertices.Count; j++)
+                    {
+                        if (Grafs[i].Vertices[j].data == edge.start.data)
+                        {
+                            Grafs[i].Edges.Add(edge);
+                            Grafs[i].Vertices.Add(edge.end);
+                            return;
+                        }
+                        if (Grafs[i].Vertices[j].data == edge.end.data)
+                        {
+                            Grafs[i].Edges.Add(edge);
+                            Grafs[i].Vertices.Add(edge.start);
+                            return;
+                        }
+                    }
+                }
+            }
+            if(newVerticesCount == 0)
+            {
+                if (NewVerticesCount(edge) == 0) return;
+                for(int i = 0; i < Grafs.Count; i++)
+                {
+                    if (Grafs[i].NewVerticesCount(edge) == 0) return;
+                }
+
+                if (NewVerticesCount(edge) == 1)
+                {
+                    int grafToJoinId = 0;
+                    for (int i = 0; i < Grafs.Count; i++)
+                    {
+                        if (Grafs[i].NewVerticesCount(edge) == 1) grafToJoinId = i;
+                    }
+                    Join(Grafs[i]);
                     return;
                 }
-                Vertices.Add(edge.start);
+
+                Graf firstGrafToJoin;
+                Graf secondGrafToJoin;
+                for (int i = 0; i < Grafs.Count; i++)
+                {
+                    if (Grafs[i].NewVerticesCount(edge) == 1 && firstGrafToJoin == null)
+                    {
+                        firstGrafToJoin = Grafs[i];
+                    }
+                    else if(Grafs[i].NewVerticesCount(edge) == 1)
+                    {
+                        secondGrafToJoin = Grafs[i];
+                        Grafs.Remove(i);
+                    }
+                }
+                firstGrafToJoin.Edges.Add(edge);
+                firstGrafToJoin.Join(secondGrafToJoin);
             }
         }
         void Join(Graf graf)
         {
+            // Join 2 grafs when adding an edge across different grafs
             for(int i = 0; i < graf.Edges.Count; i++)
             {
-                Add(graf.Edges[i]);
+                Edges.Add(graf.Edges[i]);
+            }
+            for (int i = 0; i < graf.Vertices.Count; i++)
+            {
+                Vertices.Add(graf.Vertices[i]);
             }
         }
-
     }
 }
